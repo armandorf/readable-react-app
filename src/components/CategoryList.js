872f0fb81da80b4865fa5f7b457747e80dff133b
Pedro Flores <pflores@apttus.com>
2017-11-14 10:49:00 -0800

    feat: Refactor the way props are accessed

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -6,19 +6,21 @@ import { DropdownButton, MenuItem, ButtonToolbar } from 'react-bootstrap';
 class CategoryList extends Component {
 
   render() {
+    const { categories, posts } = this.props;
+
     return (
       <div>
 
         <h1>Categories</h1>
         <ul>
-          {this.props.categories && this.props.categories.map(category => (
+          {categories && categories.map(category => (
             <li key={category.name}>
               <Link to={`/${category.path}`}>{category.name}</Link>
             </li>
           ))}
         </ul>
 
-        <PostList posts={this.props.posts} />
+        <PostList posts={posts} />
   
       </div>
     );
