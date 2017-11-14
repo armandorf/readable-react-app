3f1bc8b7c9e3d9a658345276409152de9c6aa7b9
Pedro Flores <pflores@apttus.com>
2017-11-14 14:02:19 -0800

    feat: Implement functionality to create a new post using a modal in the PostList component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,44 +2,154 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Post from './Post';
 import sortBy from 'sort-by';
-import { DropdownButton, MenuItem, Media } from 'react-bootstrap';
+import { ButtonToolbar,
+         DropdownButton,
+         MenuItem,
+         Media,
+         Button,
+         Modal,
+         FormGroup,
+         ControlLabel,
+         FormControl,
+         HelpBlock } from 'react-bootstrap';
+import uniqid from 'uniqid';
+import { username } from '../utils/requestOptions';
 
 class PostList extends Component {
   state = {
-    sortFlagRender: false,
+    sortFlag: false,
+    showModal: false,
+    hasSelectedCategory: true,
+    newPost: {
+      category: 'Select a category',
+    },
   };
 
   sortPostsBy = key => () => {
     this.props.posts.sort(sortBy(key));
-    this.setState({ sortFlagRender: !this.state.sortFlagRender }); // trigger a re-render
+    this.setState({ sortFlag: !this.state.sortFlag });
+  };
+
+  closeModal = () => {
+    this.setState({ showModal: false });
+  };
+
+  openModal = () => {
+    this.setState({ showModal: true });
+  };
+
+  assignTitleValue = e => {
+    this.setState({
+      newPost: {
+        ...this.state.newPost,
+        title: e.target.value,
+      },
+    });
+  };
+
+  assignBodyValue = e => {
+    this.setState({
+      newPost: {
+        ...this.state.newPost,
+        body: e.target.value,
+      },
+    });
+  };
+
+  assignCategoryValue = e => {
+    this.setState({
+      newPost: {
+        ...this.state.newPost,
+        category: e.target.value,
+      },
+    });
+  };
+
+  createPostAndCloseModal = () => {
+    if (this.state.newPost.category === 'Select a category') {
+      this.setState({ hasSelectedCategory: false });
+      return;
+    }
+
+    const newPost = {
+      ...this.state.newPost,
+      id: uniqid(),
+      timestamp: Date.now(),
+      author: username,
+    };
+    this.props.createPost(newPost);
+
+    this.closeModal();
   };
 
   render() {
+
     return (
       <div>
 
         <h1>Posts</h1>
-        <DropdownButton dropup bsSize="small" bsStyle='default' title='Sort by' id='sort-by-buttons'>
-          <MenuItem onSelect={this.sortPostsBy('timestamp')}>Date Created</MenuItem>
-          <MenuItem onSelect={this.sortPostsBy('-voteScore')}>Vote Score</MenuItem>
-          <MenuItem onSelect={this.sortPostsBy('title')}>Title</MenuItem>
-        </DropdownButton>
+        <ButtonToolbar>
+          <DropdownButton bsSize="small" bsStyle='default' title='Sort by' id='sort-by-buttons'>
+            <MenuItem onSelect={this.sortPostsBy('timestamp')}>Date Created</MenuItem>
+            <MenuItem onSelect={this.sortPostsBy('-voteScore')}>Vote Score</MenuItem>
+            <MenuItem onSelect={this.sortPostsBy('title')}>Title</MenuItem>
+          </DropdownButton>
+          <Button bsSize="small" bsStyle="primary" onClick={this.openModal}>Create new post</Button>
+        </ButtonToolbar>
 
-        <Media.List>
+        <Media.List className='post-list'>
           {this.props.posts && this.props.posts.map(post => (
             <Media.ListItem key={post.id}>
               <Post post={post} isListItem={true} />
             </Media.ListItem>
           ))}
         </Media.List>
-        
-        {/*<ul>*/}
-          {/*{this.props.posts && this.props.posts.map(post => (*/}
-            {/*<li key={post.id}>*/}
-              {/*<Link to={`/${post.category}/${post.id}`}>{post.title}</Link> created on {new Date(post.timestamp).toDateString()} with vote score {post.voteScore}*/}
-            {/*</li>*/}
-          {/*))}*/}
-        {/*</ul>*/}
+
+        <Modal show={this.state.showModal} onHide={this.closeModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Edit post</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form>
+              <FormGroup controlId="titleEditing">
+                <ControlLabel>Title</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.title}
+                  onChange={this.assignTitleValue}
+                />
+              </FormGroup>
+              <FormGroup controlId="bodyEditing">
+                <ControlLabel>Body</ControlLabel>
+                <FormControl
+                  componentClass="textarea"
+                  value={this.state.body}
+                  onChange={this.assignBodyValue}
+                />
+              </FormGroup>
+              <FormGroup controlId="categoryEditing">
+                <ControlLabel>Category</ControlLabel>
+                <FormControl
+                  componentClass="select"
+                  value={this.state.newPost.category}
+                  onChange={this.assignCategoryValue}
+                >
+                  <option key='categorySelection' value='Select a category'>Please select a category</option>
+                  {this.props.categories && this.props.categories.map(category => (
+                    <option key={category.path} value={category.name}>
+                      {category.name}
+                    </option>
+                  ))}
+                </FormControl>
+                {!this.state.hasSelectedCategory && (<HelpBlock>You must select a category</HelpBlock>)}
+              </FormGroup>
+            </form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.closeModal}>Close</Button>
+            <Button bsStyle="primary" onClick={this.createPostAndCloseModal}>Save changes</Button>
+          </Modal.Footer>
+        </Modal>
 
       </div>
     );
